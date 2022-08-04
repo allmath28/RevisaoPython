29568f76d17d45cecb0b9965cef2a6fa38849709
Allan Matheus <allan.msbl@gmail.com>
2022-08-03 23:03:06 -0300

    implementação do laço for

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,21 +3,27 @@ print('Bem Vindo ao jogo de advinhação!')
 print('********************************')
 
 numero_secreto = 52
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite o seu número: "))
+for rodada in range(total_de_tentativas+1):
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    print(f'Rodada {rodada} de {total_de_tentativas}')
 
-print(f'Você digitou {chute}')
+    chute = int(input("Digite o seu número: "))
 
-if acertou:
-    print(f'Você acertou')
-else:
-    if maior:
-        print(f'Você errou! O seu chute foi maior que o número secreto.')
-    elif menor:
-        print(f'Você errou! O seu chute foi menor que o número secreto.')
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    print(f'Você digitou {chute}')
+
+    if acertou:
+        print(f'Você acertou')
+    else:
+        if maior:
+            print(f'Você errou! O seu chute foi maior que o número secreto.')
+        elif menor:
+            print(f'Você errou! O seu chute foi menor que o número secreto.')
 
 print('Fim de jogo!')
